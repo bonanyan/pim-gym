6b9b564f3c6299822f354eafe8b968d83f0b8941
Bonan Yan <bonan.yan@outlook.com>
2024-01-18 09:28:59 +0800

    update

--- sphinx_doc/source/conf.py
+++ sphinx_doc/source/conf.py
@@ -22,9 +22,8 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-
+html_theme = 'furo'
 html_context = {
    "default_mode": "light"
 }
+html_static_path = ['_static']
